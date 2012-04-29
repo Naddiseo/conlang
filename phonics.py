0c07044b13f9de0a02832c37e1a7fcab145d009c
Richard Eames <naddiseo@gmail.com>
2012-04-29 00:09:00 -0600

    - Initial python stuff

---
+++ phonics.py
@@ -0,0 +1,92 @@
+class Phoneme(object):
+	def __init__(self, ipa, ortho):
+		self.ipa = ipa
+		self.ortho = ortho
+	
+	def __unicode__(self):
+		return self.ipa
+	
+	def __str__(self):
+		return self.ortho
+
+class Frontness(object):
+	FRONT = 1
+	CENTER = 2
+	BACK = 3
+
+class Height(object):
+	HIGH = 1
+	MID = 2
+	LOW = 3
+
+class Vowel(Phoneme):
+	def __init__(self, ipa, ortho, frontness, height, rounded, nasalized):
+		super(Vowel, self).__init__(ipa, ortho)
+		self.frontness = frontness
+		self.height = height
+		self.rounded = rounded
+		self.nasalized = nasalized
+
+class Position(object):
+	BILABIAL = 1
+	LABIODENTAL = 2
+	ALVEOLAR = 3
+	POSTALVEOLAR = 4
+	PALATAL = 5
+	VELAR = 6
+	UVULAR = 7
+	GLOTTAL = 8
+	EPIGLOTTAL = 9
+
+class Articulation(object):
+	NASAL = 1
+	PLOSIVE = 2
+	FRICATIVE = 3
+	APPROX = 4
+	LATERAL = 5
+
+class Consonant(Phoneme):
+	def __init__(self, ipa, ortho, position, articulation, voiced):
+		super(Consonant, self).__init__(ipa, ortho)
+		self.position = position
+		self.articulation = articulation
+		self.voiced = voiced
+
+PHONEMES = (
+	Vowel(u'i', 'i', Frontness.FRONT, Height.HIGH, False, False),
+	Vowel(u'u', 'u', Frontness.BACK, Height.HIGH, True, False),
+	Vowel(u'\u025C', 'e', Frontness.FRONT, Height.MID, False, False),
+	Vowel(u'\u0254', 'o', Frontness.BACK, Height.MID, True, False),
+	Vowel(u'a', 'a', Frontness.FRONT, Height.LOW, False, False),
+	Consonant(u'm', 'm', Position.BILABIAL, Articulation.NASAL, True),
+	Consonant(u'p', 'p', Position.BILABIAL, Articulation.PLOSIVE, False),
+	Consonant(u'b', 'b', Position.BILABIAL, Articulation.PLOSIVE, True),
+	Consonant(u'f', 'f', Position.LABIODENTAL, Articulation.FRICATIVE, False),
+	Consonant(u'v', 'v', Position.LABIODENTAL, Articulation.FRICATIVE, True),
+	Consonant(u'n', 'n', Position.ALVEOLAR, Articulation.NASAL, True),
+	Consonant(u't', 't', Position.ALVEOLAR, Articulation.PLOSIVE, False),
+	Consonant(u'd', 'd', Position.ALVEOLAR, Articulation.PLOSIVE, True),
+	Consonant(u's', 's', Position.ALVEOLAR, Articulation.FRICATIVE, False),
+	Consonant(u'z', 'z', Position.ALVEOLAR, Articulation.FRICATIVE, True),
+	Consonant(u'r', 'r', Position.ALVEOLAR, Articulation.APPROX, True),
+	Consonant(u'l', 'l', Position.ALVEOLAR, Articulation.LATERAL, True),
+	Consonant(u'\u0283', 'sh', Position.POSTALVEOLAR, Articulation.FRICATIVE, False),
+	Consonant(u'\u0292', 'zh', Position.POSTALVEOLAR, Articulation.FRICATIVE, True),
+	Consonant(u'\u014B', 'ng', Position.VELAR, Articulation.NASAL, True),
+	Consonant(u'k', 'k', Position.VELAR, Articulation.PLOSIVE, False),
+	Consonant(u'g', 'g', Position.VELAR, Articulation.PLOSIVE, True),
+	Consonant(u'x', 'kh', Position.VELAR, Articulation.FRICATIVE, False),
+	Consonant(u'\u0263', 'gh', Position.VELAR, Articulation.FRICATIVE, True),
+	Consonant(u'q', 'q', Position.UVULAR, Articulation.PLOSIVE, False),
+	Consonant(u'\u0262', 'c', Position.UVULAR, Articulation.PLOSIVE, True),
+	Consonant(u'\u03C7', 'qh', Position.UVULAR, Articulation.FRICATIVE, False),
+	Consonant(u'\u0281', 'ch', Position.UVULAR, Articulation.FRICATIVE, True),
+	Consonant(u'\u0294', '\'', Position.GLOTTAL, Articulation.FRICATIVE, False),
+	Consonant(u'h', 'h', Position.GLOTTAL, Articulation.FRICATIVE, False),
+)
+
+def phoneme(str):
+	for ph in PHONEMES:
+		if ph.ortho == str:
+			return ph
+	raise ValueError("'{}' is not a valid phoneme".format(str))
---
+++ syllables.py
@@ -0,0 +1,161 @@
+from __future__ import division
+import codecs,sys
+from itertools import combinations, chain, permutations
+from phonics import PHONEMES, phoneme as p, \
+	Vowel, Consonant, Articulation as A, Position
+
+art_pn = [A.PLOSIVE, A.NASAL]
+art_al = [A.APPROX, A.LATERAL]
+art_fp = [A.FRICATIVE, A.PLOSIVE]
+
+def valid_cluster(c):
+	l = len(c)
+
+	if l < 2: return True
+	if l > 3: return False
+
+	pairs = [(c[0], c[1])]
+	adj_pairs = [pairs[0]]
+
+	if l == 3:
+		pairs += [(c[1], c[2]), (c[0], c[2])]
+		adj_pairs += [pairs[1]]
+
+	for (c1, c2) in pairs:
+		if c1 == c2:
+			return False
+		if c1.articulation == c2.articulation:
+			if c1.articulation in art_pn:
+				return False
+			if c1.voiced is not c2.voiced:
+				return False
+		if c1.articulation in art_al and c2.articulation in art_al:
+			return False
+
+		if c1.articulation == A.NASAL and c2.articulation in art_al:
+			# nl / ngl / nr / mr
+			return False
+
+	for (c1, c2) in adj_pairs:
+		if c1.voiced is not c2.voiced:
+			if c1.articulation in art_fp and c2.articulation in art_fp:
+				return False
+		if c1.articulation == A.NASAL and c2.articulation == A.FRICATIVE:
+			return False
+		if c1.articulation == A.FRICATIVE \
+			and c1.position == Position.ALVEOLAR \
+			and	c2.position == Position.UVULAR:
+			return False
+		if c1.position > Position.ALVEOLAR and c2.position > Position.ALVEOLAR:
+			if c1.articulation == c2.articulation:
+				return False
+
+		if c1.articulation in art_al and c2.articulation == A.FRICATIVE:
+			return False
+
+	if l == 3:
+		c1,c2,c3 = c
+
+		if c2.articulation in art_pn:
+			if c3.position >= c2.position: # (c)kx / (c)kqh / (c)nqg
+				return False
+
+	return True
+
+def valid_codas(c):
+	l = len(c)
+
+	if l < 2: return True
+
+	pairs = [(c[0], c[1])]
+	adj_pairs = [pairs[0]]
+
+	if l == 3:
+		pairs += [(c[1], c[2]), (c[0], c[2])]
+		adj_pairs += [pairs[1]]
+
+	for pair in pairs:
+		c1,c2 = pair
+
+		if c1.articulation == A.PLOSIVE:
+			if c2.articulation == A.NASAL:
+				return False
+		if c2.articulation in art_al: # No approx in syllable final
+			return False
+
+	return True
+
+def valid_onsets(c):
+	l = len(c)
+
+	if l < 2: return True
+
+	pairs = [(c[0], c[1])]
+	adj_pairs = [pairs[0]]
+
+	if l == 3:
+		pairs += [(c[1], c[2]), (c[0], c[2])]
+		adj_pairs += [pairs[1]]
+
+	for pair in pairs:
+		c1,c2 = pair
+		if c[0].articulation in art_al + [A.NASAL]: # approx in syllable initial
+			if c[1].articulation == A.PLOSIVE:
+				return False
+
+
+
+	return True
+
+
+CONSONS = [ph for ph in PHONEMES if isinstance(ph, Consonant)]
+GLOTTALS = [ph for ph in CONSONS if ph.position is Position.GLOTTAL]
+
+V = [ph for ph in PHONEMES if isinstance(ph, Vowel)]
+C = [ph for ph in CONSONS if ph.position is not Position.GLOTTAL]
+
+i = p('i')
+DIPH = [
+	[p(letter), i] for letter in ('a', 'o', 'e')
+] + [
+	[i, p(letter)] for letter in ('a', 'o', 'e', 'u')
+]
+
+CLUSTERS = chain(
+	combinations(C, 1),
+	combinations(C, 2),
+)
+
+nuclei = [[ph] for ph in V] + DIPH
+codas = filter(valid_cluster, CLUSTERS) + [[]]
+onsets = codas + [[ph] for ph in GLOTTALS]
+
+codas = filter(valid_codas, codas)
+onsets = filter(valid_onsets, onsets)
+
+print len(nuclei), len(codas), len(onsets)
+print '=', len(nuclei) * len(codas) * len(onsets)
+
+syllables = set()
+o = len(onsets) +len(nuclei)
+t = o
+i = 0
+for onset in onsets:
+	for nucleus in nuclei:
+		sys.stderr.write('{:.2f}\r'.format(i/t*100))
+		i += 1
+		for coda in codas:
+			syllables.update([u'{}{}{}\n'.format(
+				u''.join(o.ipa for o in onset), 
+				u''.join(n.ipa for n in nucleus), 
+				u''.join(c.ipa for c in coda)
+			)])
+		t = o+len(syllables)
+
+with codecs.open('syls.txt', 'w', encoding = 'utf-8') as f:	
+	syllables = sorted(syllables)
+	syllables = sorted(syllables, key=len)
+	for syllable in syllables:
+		sys.stderr.write('{:.2f}\r'.format(i/t*100))
+		i += 1
+		f.write(syllable)
